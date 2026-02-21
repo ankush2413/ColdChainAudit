604aef8c36093874dcebea253721f4a3fa63be77
Ankush Mangal <ankush@Ankushs-MacBook-Air.local>
2026-02-21 19:39:22 +0530

    Initialize project

---
+++ src/main/java/com/supplychain/ColdChainAudit_API/ColdChainAuditApiApplication.java
@@ -0,0 +1,13 @@
+package com.supplychain.ColdChainAudit_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColdChainAuditApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColdChainAuditApiApplication.class, args);
+	}
+
+}
